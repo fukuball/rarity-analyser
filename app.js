a92f160924b811c8cf4931914dcaeb168df16ee1
Fukuball Lin <fukuball@gmail.com>
2021-09-12 00:12:08 +0800

    Add some style

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var livereload = require('easy-livereload');
+
+if (app.get('env') === 'development') {
+  app.use(livereload({
+    app: app
+  }));
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
