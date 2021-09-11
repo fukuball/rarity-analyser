54d375750d9b193330d42ec2055ca9f0eb4cdec7
Fukuball Lin <fukuball@gmail.com>
2021-09-12 00:33:19 +0800

    Add config file

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    app_name: 'Cool Rarity',
+    storage_prefix: '@CoolRarity',
+    ga: '',
+    collection_file_name: '',
+};
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+var Config = require('../config/config');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CoolRarity' });
+  res.render('index', { title: Config.app_name });
 });
 
 module.exports = router;
