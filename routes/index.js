baa87dfb2fcb8c23593e5764261707af5504f3ee
Fukuball Lin <fukuball@gmail.com>
2021-09-12 17:38:52 +0800

    Add sorting

--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,17 @@ const db = new Database(databasePath);
 router.get('/', function(req, res, next) {
 
   let search = req.query.search;
+  let orderBy = req.query.order_by;
   let page = req.query.page;
 
   let offset = 0;
   let limit = 100;
+
+  if (orderBy == 'rarity' || orderBy == 'id') {
+    orderBy = orderBy;
+  } else {
+    orderBy = 'rarity';
+  }
 
   if (!_.isEmpty(page)) {
     page = parseInt(page);
@@ -29,20 +36,25 @@ router.get('/', function(req, res, next) {
   }
 
   let totalPunkCount = 0
-  
-
   let punks = null;
+  let orderByStmt = '';
+  if (orderBy == 'rarity') {
+    orderByStmt = 'ORDER BY punk_scores.rarity_rank ASC';
+  } else {
+    orderByStmt = 'ORDER BY punks.id ASC';
+  }
+
   if (!_.isEmpty(search)) {
     totalPunkCount = db.prepare('SELECT COUNT(id) as punk_total FROM punks WHERE punks.id LIKE ?').get('%'+search+'%').punk_total;
-    punks =  db.prepare('SELECT punks.*, punk_scores.rarity_rank FROM punks INNER JOIN punk_scores ON (punks.id = punk_scores.punk_id) WHERE punks.id LIKE ? LIMIT ?,?').all('%'+search+'%', offset, limit);
+    punks =  db.prepare('SELECT punks.*, punk_scores.rarity_rank FROM punks INNER JOIN punk_scores ON (punks.id = punk_scores.punk_id) WHERE punks.id LIKE ? '+orderByStmt+' LIMIT ?,?').all('%'+search+'%', offset, limit);
   } else {
     totalPunkCount = db.prepare('SELECT COUNT(id) as punk_total FROM punks').get().punk_total;
-    punks = db.prepare('SELECT punks.*, punk_scores.rarity_rank FROM punks INNER JOIN punk_scores ON (punks.id = punk_scores.punk_id) LIMIT ?,?').all(offset, limit);
+    punks = db.prepare('SELECT punks.*, punk_scores.rarity_rank FROM punks INNER JOIN punk_scores ON (punks.id = punk_scores.punk_id) '+orderByStmt+' LIMIT ?,?').all(offset, limit);
   }
 
   let totalPage =  Math.ceil(totalPunkCount/limit);
 
-  res.render('index', { title: config.app_name, punks: punks, totalPage: totalPage, search: search, page: page, _:_ });
+  res.render('index', { title: config.app_name, punks: punks, totalPage: totalPage, search: search, orderBy, orderBy, page: page, _:_ });
 });
 
 module.exports = router;
