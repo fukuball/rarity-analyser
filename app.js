7d69c1a6edf466aac78a31892431890ef36553ac
Fukuball Lin <fukuball@gmail.com>
2021-09-13 15:56:53 +0800

    Add collection name

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.locals.app_name = config.app_name;
 app.locals.ga_code = config.ga;
 app.locals.collection_contract_address = config.collection_contract_address;
+app.locals.collection_name = config.collection_name;
 
 app.use('/', indexRouter);
 app.use('/punk', punksRouter);
--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ const config = {
     storage_prefix: '@CoolRarity',
     collection_file_name: 'collection.json',
     collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
+    collection_name: '10k One Day Punks',
     sqlite_file_name: 'database.sqlite',
     ga: 'G-BW69Z04YTP',
     main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png'
