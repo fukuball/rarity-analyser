16fb6c52767c200b31402e0ba6992c714c599968
Fukuball Lin <fukuball@gmail.com>
2021-09-14 03:34:37 +0800

    Add json api

--- routes/punks.js
+++ routes/punks.js
@@ -51,7 +51,7 @@ router.get('/:id', function(req, res, next) {
   res.render('punk', { 
     title: title,
     ogUrl: req.protocol + '://' + req.get('host') + req.originalUrl,
-    ogImage: punk.image.replace('ipfs://', 'https://ipfs.io/ipfs/'),
+    ogImage: punk ? punk.image.replace('ipfs://', 'https://ipfs.io/ipfs/'): config.main_og_image,
     punk: punk, 
     punkScore: punkScore, 
     allTraitTypes: allTraitTypes, 
@@ -62,6 +62,69 @@ router.get('/:id', function(req, res, next) {
     _: _,
     md: md
   });
+});
+
+router.get('/:id/json', function(req, res, next) {
+  let punkId = req.params.id;
+  let punk = db.prepare('SELECT punks.*, punk_scores.rarity_rank FROM punks INNER JOIN punk_scores ON (punks.id = punk_scores.punk_id) WHERE punks.id = ?').get(punkId);
+  
+  if (_.isEmpty(punk)) {
+    res.end(JSON.stringify({
+      status: 'fail',
+      message: 'not_exist',
+    }));
+  }
+
+  let punkTraits = db.prepare('SELECT punk_traits.trait_type_id, trait_types.trait_type, punk_traits.value  FROM punk_traits INNER JOIN trait_types ON (punk_traits.trait_type_id = trait_types.id) WHERE punk_traits.punk_id = ?').all(punkId);
+  let punkScore = db.prepare('SELECT punk_scores.* FROM punk_scores WHERE punk_scores.punk_id = ?').get(punkId);
+  let allTraitTypes = db.prepare('SELECT trait_types.* FROM trait_types').all();
+  
+  let punkTraitsData = [];
+  let punkTraitIDs = [];
+  punkTraits.forEach(punkTrait => {
+    let percentile = punkScore['trait_type_'+punkTrait.trait_type_id+'_percentile'];
+    let rarity_score = punkScore['trait_type_'+punkTrait.trait_type_id+'_rarity'];
+    punkTraitsData.push({
+      trait_type: punkTrait.trait_type,
+      value: punkTrait.value,
+      percentile: percentile,
+      rarity_score: rarity_score,
+    });
+    punkTraitIDs.push(punkTrait.trait_type_id);
+  });
+
+  let missingTraitsData = [];
+  allTraitTypes.forEach(traitType => {
+    if (!punkTraitIDs.includes(traitType.id)) {
+      let percentile = punkScore['trait_type_'+traitType.id+'_percentile'];
+      let rarity_score = punkScore['trait_type_'+traitType.id+'_rarity'];
+      missingTraitsData.push({
+        trait_type: traitType.trait_type,
+        percentile: percentile,
+        rarity_score: rarity_score,
+      });
+    }
+  });
+  
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({
+    status: 'success',
+    message: 'success',
+    punk: {
+      id: punk.id,
+      name: punk.name,
+      image: punk.image,
+      attributes: punkTraitsData,
+      missing_traits: missingTraitsData,
+      trait_count: {
+        count: punkScore.trait_count,
+        percentile: punkScore.trait_count_percentile,
+        rarity_score: punkScore.trait_count_rarity
+      },
+      rarity_score: punkScore.rarity_sum,
+      rarity_rank: punkScore.rarity_rank
+    }
+  }));
 });
 
 module.exports = router;
