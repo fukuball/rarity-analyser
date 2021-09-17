7ede7e73cb27a598b3199c378a75c5371e03b936
Fukuball Lin <fukuball@gmail.com>
2021-09-17 18:34:17 +0800

    Add heroku app

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
     app_name: 'Cool Rarity',
-    app_description: 'A cool rarity tool.',
+    app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
     collection_file_name: 'collection.json',
     collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
     collection_name: '10k One Day Punks',
