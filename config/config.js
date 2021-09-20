d7f7fbe1c608682dbe0e52bc8bb98057a0afd14c
Fukuball Lin <fukuball@gmail.com>
2021-09-20 17:46:18 +0800

    use database

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = {
     collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
     collection_name: '10k One Day Punks',
     collection_description: '10k "One Day I\'ll Be A Punk"-punks – a homage to the one and only CryptoPunks. Holding a OneDayPunk will give you early access to PunkScapes and reserve a profile on the PunkScape website.',
-    sqlite_file_name: 'database.sqlite',
+    sqlite_file_name: 'database.sqlite.index',
     ga: 'G-BW69Z04YTP',
     main_og_image: 'https://cool-rarity.herokuapp.com/images/cool_rarity.png',
     content_image_frame: 'circle' // circle, rectangle
--- routes/indexRarityTool.js
+++ routes/indexRarityTool.js
@@ -2,10 +2,17 @@ const appRoot = require('app-root-path');
 const config = require(appRoot + '/config/config.js');
 const express = require('express');
 const router = express.Router();
+const Database = require('better-sqlite3');
 const _ = require('lodash');
+
+let databasePath = appRoot + '/config/' + config.sqlite_file_name;
+const db = new Database(databasePath);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  let projects = db.prepare('SELECT * FROM projects').all();
+
   res.render('index_rarity_tool', { 
     appTitle: config.app_name,
     appDescription: config.app_description,
@@ -14,6 +21,7 @@ router.get('/', function(req, res, next) {
     ogUrl: req.protocol + '://' + req.get('host') + req.originalUrl,
     ogImage: config.main_og_image,
     activeTab: 'rarity',
+    projects: projects,
     _:_ 
   });
 });
