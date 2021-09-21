d799a19b46b27c45eeeb740aa87892b432e9c719
Fukuball Lin <fukuball@gmail.com>
2021-09-21 22:10:41 +0800

    update

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ const config = {
     sqlite_file_name: 'database.sqlite',
     ga: 'G-BW69Z04YTP',
     main_og_image: 'https://badkids.coolrarity.com/images/badkids.jpg',
-    page_item_num: 8,
+    page_item_num: 12,
     content_image_frame: 'rectangle' // circle, rectangle
 };
 
