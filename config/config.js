fef9ba0e1561a723d4ed43c53c345d341fb9671a
Fukuball Lin <fukuball@gmail.com>
2021-09-21 18:37:14 +0800

    add cdn

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
     app_name: 'BadKids Rarity',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
-    collection_file_name: 'badkids_collection.json',
+    collection_file_name: 'badkids_cdn_collection.json',
     collection_contract_address: '0x4498118b7e1ab8bc9352037cf71f66b3151f4351',
     collection_name: 'Bad Kids Alley',
     collection_description: '8,888 collectibles illustrated by @UnstableKido. Our goal is straight forward, We are building a global brand. We want see you bad kids everywhere! Our team is built up of fantastic community leaders and well-connected humans from all around the globe.',
