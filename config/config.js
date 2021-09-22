befee57d35a565b78880d7ff55fa74c2399668d0
Fukuball Lin <fukuball@gmail.com>
2021-09-23 00:16:14 +0800

    Add config file

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ const config = {
     collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
     collection_name: '10k One Day Punks',
     collection_description: '10k "One Day I\'ll Be A Punk"-punks – a homage to the one and only CryptoPunks. Holding a OneDayPunk will give you early access to PunkScapes and reserve a profile on the PunkScape website.',
+    coolection_id_from: 0,
     sqlite_file_name: 'database.sqlite',
     ga: 'G-BW69Z04YTP',
     main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png',
