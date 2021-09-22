82abda632995a9aaac8708a31cad4ee04b7b2015
Fukuball Lin <fukuball@gmail.com>
2021-09-23 03:43:44 +0800

    update

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const config = {
     collection_contract_address: '0x9f4292bd05a5c89f007bdef3a95fc32bc1021b99',
     collection_name: 'Frogs In Disguise',
     collection_description: 'All across the land, frogs are being snatched up by storks. King Ribbit needs YOUR help to save the remaining 10,000 frogs in disguise. Join his kingdom today and check out the mysterious world of Frognolia.',
-    coolection_id_from: 0,
+    coolection_id_from: 1,
     sqlite_file_name: 'database.sqlite',
     ga: 'G-BW69Z04YTP',
     main_og_image: 'https://frogsindisguise.com/_next/static/image/assets/hero.3051596d416e2508d26f6ca84cb121a1.jpg',
