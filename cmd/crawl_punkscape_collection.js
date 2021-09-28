067ddc0e87dcca7100ce13eb3528cd7ad2ad5927
Fukuball Lin <fukuball@gmail.com>
2021-09-28 21:29:47 +0800

    update

--- cmd/crawl_punkscape_collection.js
+++ cmd/crawl_punkscape_collection.js
@@ -4,12 +4,14 @@ const request = require('sync-request');
 
 const outputPath = appRoot + '/config/punkscape_collection.json';
 
-/*if (fs.existsSync(outputPath)) {
-    let collectionData = require(outputPath);
-    console.log(collectionData.length);
-    return;
-}*/
-
+let collectionData = [];
+let existItem = [];
+if (fs.existsSync(outputPath)) {
+    collectionData = require(outputPath);
+    collectionData.forEach(element => {
+        existItem.push(element.id); 
+    });
+}
 
 let from = 0;
 let total = 10000;
@@ -18,18 +20,20 @@ fs.appendFileSync(outputPath, "[\n");
 
 for (i = from; i < total; i++) {
     console.log("Process: #" + i);
-    let url = 'https://bafybeiclqgyaqdgbi3goa7mz5ukp5nix2u3y5pkc22sfaiv5i5aygzcp4i.ipfs.dweb.link/'+i+'/metadata.json';
-    let res = request('GET', url);
-    if (res.statusCode == 200) {
-        let data = res.getBody('utf8');
-        data = JSON.parse(data);
-        data['id'] = i;
-        data = JSON.stringify(data);
-        console.log(data);
-        if (i == total-1) {
-            fs.appendFileSync(outputPath, data+"\n");
-        } else {
-            fs.appendFileSync(outputPath, data+",\n");
+    if (!existItem.includes(i)) {
+        let url = 'https://bafybeiclqgyaqdgbi3goa7mz5ukp5nix2u3y5pkc22sfaiv5i5aygzcp4i.ipfs.dweb.link/'+i+'/metadata.json';
+        let res = request('GET', url);
+        if (res.statusCode == 200) {
+            let data = res.getBody('utf8');
+            data = JSON.parse(data);
+            data['id'] = i;
+            data = JSON.stringify(data);
+            console.log(data);
+            if (i == total-1) {
+                fs.appendFileSync(outputPath, data+"\n");
+            } else {
+                fs.appendFileSync(outputPath, data+",\n");
+            }
         }
     }
 }
