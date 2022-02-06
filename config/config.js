b24375c8212a6e22c9423623c7160372e8bec583
Fukuball Lin <fukuball@gmail.com>
2022-02-07 00:58:11 +0800

    0x0

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 const config = {
     app_name: 'Cool Rarity',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
-    collection_file_name: 'collection.json',
-    collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
-    collection_name: '10k One Day Punks',
-    collection_description: '10k "One Day I\'ll Be A Punk"-punks – a homage to the one and only CryptoPunks. Holding a OneDayPunk will give you early access to PunkScapes and reserve a profile on the PunkScape website.',
+    collection_file_name: '0x0_collection.json',
+    collection_contract_address: '0xf1944301a68f226796c4f6a46b98b149dcbbffc1',
+    collection_name: '0x0DAO',
+    collection_description: '0x0 is NOT an NFT project. If you love simians, Opensea’s got plenty. We on the other hand are music lovers who want to create a home for music and musicians in the metaverse…the Web3 Motown. On this adventure, stars will be born, classics will be created and soundtrack to the future will be orchestrated. Join us as we reimagine A&R, production, promotion, touring, formats, intellectual property rights and above all, making music dreams come alive.',
     collection_id_from: 0,
     ignore_traits: ['date'], 
     sqlite_file_name: 'database.sqlite',
     ga: 'G-BW69Z04YTP',
-    main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png',
-    item_path_name: 'punk',
+    main_og_image: 'https://0x0.gg/wp-content/uploads/2022/01/heroimg_141.jpg',
+    item_path_name: '0x0',
     page_item_num: 60,
     content_image_is_video: false,
-    content_image_frame: 'circle', // circle, rectangle
+    content_image_frame: 'rectangle', // circle, rectangle
     use_wallet: false
 };
 
