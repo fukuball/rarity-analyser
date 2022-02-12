96181020042589397857eb7784abca4d3cf2306c
Dan Henry <dhoff002@gmail.com>
2022-02-12 07:01:43 -0500

    dev

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 const config = {
-    app_name: 'Cool Rarity',
+    app_name: 'DeadBits Rarity',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
     collection_file_name: 'collection.json',
-    collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
-    collection_name: '10k One Day Punks',
-    collection_description: '10k "One Day I\'ll Be A Punk"-punks – a homage to the one and only CryptoPunks. Holding a OneDayPunk will give you early access to PunkScapes and reserve a profile on the PunkScape website.',
+    collection_contract_address: '0x7542E42A557C2684Adf0CAD5D511eA81a8188Bfb',
+    collection_name: 'DeadBits',
+    collection_description: 'GgRrRrarrghhh!!',
     collection_id_from: 0,
     ignore_traits: ['date'], 
     sqlite_file_name: 'database.sqlite',
     ga: 'G-BW69Z04YTP',
-    main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png',
-    item_path_name: 'punk',
+    main_og_image: 'https://ipfs.io/ipfs/QmSCkGF3YQDj3iUSURfHjuZqPw9cw7XHAut8rQkDALAjSU',
+    item_path_name: 'deadbit',
     page_item_num: 60,
     content_image_is_video: false,
-    content_image_frame: 'circle', // circle, rectangle
+    content_image_frame: 'rectangle', // circle, rectangle
     use_wallet: false
 };
 
